a7297a5e728ed74a6a2f6eaad0764c2d6ecc7084
Ice Chidi <icechidi3@gmail.com>
2025-07-04 23:21:53 +0300

    script for categories in database

--- scripts/06-seed-dataa-more.sql
+++ scripts/06-seed-dataa-more.sql
@@ -17,3 +17,39 @@ INSERT INTO subcategories (name, category_id) VALUES
 ('Motherboard', (SELECT id FROM categories WHERE name = 'Hardware')),
 ('RAM', (SELECT id FROM categories WHERE name = 'Hardware')),
 ('CPU', (SELECT id FROM categories WHERE name = 'Hardware')),
+
+
+-- Software subcategories
+('Operating System', (SELECT id FROM categories WHERE name = 'Software')),
+('Productivity', (SELECT id FROM categories WHERE name = 'Software')),
+('Security', (SELECT id FROM categories WHERE name = 'Software')),
+
+-- Accessories subcategories
+('Mouse', (SELECT id FROM categories WHERE name = 'Accessories')),
+('Keyboard', (SELECT id FROM categories WHERE name = 'Accessories')),
+('Webcam', (SELECT id FROM categories WHERE name = 'Accessories')),
+
+-- Networking subcategories
+('Routers', (SELECT id FROM categories WHERE name = 'Networking')),
+('Switches', (SELECT id FROM categories WHERE name = 'Networking')),
+('Cables', (SELECT id FROM categories WHERE name = 'Networking')),
+
+-- Storage subcategories
+('SSD', (SELECT id FROM categories WHERE name = 'Storage')),
+('HDD', (SELECT id FROM categories WHERE name = 'Storage')),
+('USB Drive', (SELECT id FROM categories WHERE name = 'Storage')),
+
+-- Printers subcategories
+('Laser', (SELECT id FROM categories WHERE name = 'Printers')),
+('Inkjet', (SELECT id FROM categories WHERE name = 'Printers')),
+('Supplies', (SELECT id FROM categories WHERE name = 'Printers')),
+
+-- Cables subcategories
+('HDMI', (SELECT id FROM categories WHERE name = 'Cables')),
+('Ethernet', (SELECT id FROM categories WHERE name = 'Cables')),
+('Power', (SELECT id FROM categories WHERE name = 'Cables')),
+
+-- Office Supplies subcategories
+('Paper', (SELECT id FROM categories WHERE name = 'Office Supplies')),
+('Pens', (SELECT id FROM categories WHERE name = 'Office Supplies')),
+('Folders', (SELECT id FROM categories WHERE name = 'Office Supplies'));
